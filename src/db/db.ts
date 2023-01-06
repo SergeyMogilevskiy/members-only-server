1e6a0c21e9ab60dcbbd644fbe7943ba7b27748f2
Serhii Mohylevskyi <s.k.mogilevskiy@gmail.com>
2023-01-06 19:14:34 +0200

    Add mongo db

---
+++ src/db/db.ts
@@ -0,0 +1,34 @@
+import { Db, MongoClient, MongoClientOptions } from 'mongodb';
+
+const DB_NAME = 'DB_NAME';
+
+interface DP_Props {
+  _dbClient: MongoClient | null;
+  connect: (url: string) => void;
+  getConnection: () => Db;
+}
+
+export const db: DP_Props = {
+  _dbClient: null,
+  connect: async function (url) {
+    const client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as MongoClientOptions);
+
+    if (!client) {
+      console.log('Something wrong with connections!');
+      process.exit(1);
+    }
+
+    this._dbClient = client;
+  },
+  getConnection: function () {
+    if (!this._dbClient) {
+      console.log('You need to call the connect() function first!');
+      process.exit(1);
+    }
+
+    return this._dbClient.db(DB_NAME);
+  },
+};
